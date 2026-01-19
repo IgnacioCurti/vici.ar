6ba3d3592de2376a106c0b6f7aa45857b08ee0a3
Kevin Masci <kevin-sor@hotmail.com>
2026-01-19 17:50:37 -0300

    Configured router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,4 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes';
-
-const router = express.Router();
-
-router.use('./auth', authRoutes); // no se si esto va aca o en un intex.ts en src/routes/index.ts
-
-const app: express.Application = express();
+import app from './app';
 
 app.listen(3000, () => {
   console.log(`Listening: http://localhost:3000`);
