94fd491f691bc0869aae06bcd84603fd7d6e93bb
Kevin Masci <kevin-sor@hotmail.com>
2026-01-19 00:25:14 -0300

    Updated client for environments

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,10 +1,17 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 import { PrismaClient } from "../../prisma/generated/client";
 
 const connectionString = process.env.DATABASE_URL
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({
+  adapter,
+  log: process.env.NODE_ENV === 'development'
+    ? ['query', 'error', 'warn']
+    : ['error']
+})
 
-export { prisma }
+export default prisma;
