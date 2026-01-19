52e1a112f5e97e160471cdaa8cad1533d4d1e6db
Kevin Masci <kevin-sor@hotmail.com>
2026-01-19 01:18:15 -0300

    Added auth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,9 @@
-import express from "express";
+import express from 'express';
+import authRoutes from './routes/auth.routes';
+
+const router = express.Router();
+
+router.use('./auth', authRoutes); // no se si esto va aca o en un intex.ts en src/routes/index.ts
 
 const app: express.Application = express();
 
