85019369cd2c21f6eb0cccfd2d2bf70e4c89c8cb
Ignacio Curti <ignaciocurtijristo@gmail.com>
2026-01-20 16:16:23 -0300

    Move constant to constants folder

--- backend/src/utils/http.response.ts
+++ backend/src/utils/http.response.ts
@@ -1,51 +1,42 @@
 import type { Response } from "express";
-
-export enum HttpStatus {
-  OK = 200,
-  CREATED =  201,
-  BAD_REQUEST = 400,
-  NOT_FOUND = 404,
-  UNAUTHORIZED = 401,
-  FORBIDDEN = 403,
-  INTERNAL_SERVER_ERROR = 500,
-}
+import { STATUS_CODES } from "../constants/httpStatusCode.js";
 
 export class HttpResponse {
   Ok(res: Response, data?: any): Response {
-    return res.status(HttpStatus.OK).json({
-      status: HttpStatus.OK,
+    return res.status(STATUS_CODES.OK).json({
+      status: STATUS_CODES.OK,
       statusMsg: "Success",
       data: data,
     });
   }
 
   NotFound(res: Response, data?: any): Response {
-    return res.status(HttpStatus.NOT_FOUND).json({
-      status: HttpStatus.NOT_FOUND,
+    return res.status(STATUS_CODES.NOT_FOUND).json({
+      status: STATUS_CODES.NOT_FOUND,
       statusMsg: "Not Found",
       error: data,
     });
   }
 
   Unauthorized(res: Response, data?: any): Response {
-    return res.status(HttpStatus.UNAUTHORIZED).json({
-      status: HttpStatus.UNAUTHORIZED,
+    return res.status(STATUS_CODES.UNAUTHORIZED).json({
+      status: STATUS_CODES.UNAUTHORIZED,
       statusMsg: "Unauthorized",
       error: data,
     });
   }
 
   Forbidden(res: Response, data?: any): Response {
-    return res.status(HttpStatus.FORBIDDEN).json({
-      status: HttpStatus.FORBIDDEN,
+    return res.status(STATUS_CODES.FORBIDDEN).json({
+      status: STATUS_CODES.FORBIDDEN,
       statusMsg: "Forbidden",
       error: data,
     });
   }
 
   Error(res: Response, data?: any): Response {
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-      status: HttpStatus.INTERNAL_SERVER_ERROR,
+    return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+      status: STATUS_CODES.INTERNAL_SERVER_ERROR,
       statusMsg: "Internal server error",
       error: data,
     });
