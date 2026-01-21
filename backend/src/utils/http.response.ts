fad2c4ce8c501b53245cba09941442be5f66b23c
Kevin Masci <kevin-sor@hotmail.com>
2026-01-21 01:56:06 -0300

    Refactor responses

--- backend/src/utils/http.response.ts
+++ backend/src/utils/http.response.ts
@@ -2,43 +2,46 @@ import type { Response } from "express";
 import { STATUS_CODES } from "../constants/httpStatusCode.js";
 
 export class HttpResponse {
-  Ok(res: Response, message?: string, data?: any): Response {
+  Ok<T>(res: Response, message = "Success", data?: T): Response {
     return res.status(STATUS_CODES.OK).json({
       status: STATUS_CODES.OK,
-      statusMsg: message && "Success",
-      data: data,
+      message,
+      data,
     });
   }
 
-  NotFound(res: Response, message?: string, data?: any): Response {
+  NotFound(res: Response, message = "Not found"): Response {
     return res.status(STATUS_CODES.NOT_FOUND).json({
       status: STATUS_CODES.NOT_FOUND,
-      statusMsg: message && "Not Found",
-      error: data,
+      message,
     });
   }
 
-  Unauthorized(res: Response, message?: string, data?: any): Response {
+  Unauthorized(res: Response, message = "Unauthorized"): Response {
     return res.status(STATUS_CODES.UNAUTHORIZED).json({
       status: STATUS_CODES.UNAUTHORIZED,
-      statusMsg: message && 'Unauthorized',
-      error: data,
+      message,
     });
   }
 
-  Forbidden(res: Response, message?: string, data?: any): Response {
+  Forbidden(res: Response, message = "Forbidden"): Response {
     return res.status(STATUS_CODES.FORBIDDEN).json({
       status: STATUS_CODES.FORBIDDEN,
-      statusMsg: message && "Forbidden",
-      error: data,
+      message,
     });
   }
 
-  Error(res: Response, message?: string, data?: any): Response {
+  Error(res: Response, message = "Internal server error"): Response {
     return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
       status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-      statusMsg: message && "Internal server error",
-      error: data,
+      message,
+    });
+  }
+
+  BadRequest(res: Response, message = "Bad request"): Response {
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      status: STATUS_CODES.BAD_REQUEST,
+      message,
     });
   }
 }
