0df4a353bba1dbac885a05116559df86dbc3e46b
Kevin Masci <kevin-sor@hotmail.com>
2026-01-21 17:56:57 -0300

    Add auth protection to endpoint

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js";
+import { auth } from "../middleware/middleware.js";
 
 const router = Router();
 
-router.get("/:id", userController.getUserById.bind(userController));
+router.get("/:id", auth, userController.getUserById.bind(userController));
 
 export default router;
