161cc2b0a3f0f44668ec08ab4814ecb761f1a752
Kevin Masci <kevin-sor@hotmail.com>
2026-01-22 19:54:34 -0300

    Add heroui navbar

---
+++ frontend/src/app/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+/* import { AppNavbar } from './Navbar';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen bg-background">
+      <AppNavbar />
+      <main>{children}</main>
+    </div>
+  );
+} */
