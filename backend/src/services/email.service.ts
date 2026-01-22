0006d3a06bc9b3ebad66f2a66ddf93b605f87493
Kevin Masci <kevin-sor@hotmail.com>
2026-01-22 05:12:52 -0300

    Create email service

---
+++ backend/src/services/email.service.ts
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+import { verificationEmailTemplate } from '../utils/verificationEmailTemplate.js';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+});
+
+export class EmailService {
+  async sendVerificationEmail(email: string, code: string): Promise<void> {
+    const verificationUrl = `${process.env.FRONTEND_URL}/verify-email?code=${code}`;
+
+    await transporter.sendMail({
+      from: `"Vici.ar " <${process.env.SMTP_USER}>`,
+      to: email,
+      subject: "Email verification",
+      html: verificationEmailTemplate(code, verificationUrl),
+    });
+  }
+}
+
+export default new EmailService();
