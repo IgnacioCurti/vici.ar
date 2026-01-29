cde17c9cb332167cadb15a0043dba2353a7e8fea
Kevin Masci <kevin-sor@hotmail.com>
2026-01-29 19:26:26 -0300

    Move providers to main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppNavbar from "./app/components/layout/navbar";
 import Home from "./app/components/pages/Home";
 // import Login from "./app/pages/Login";
-import { HeroUIProvider } from "@heroui/react";
 
 function App() {
   return (
-    <HeroUIProvider>
-      <Router>
-        <div className="app">
-          <AppNavbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/login" element={<Login />} /> */}
-          </Routes>
-        </div>
-      </Router>
-    </HeroUIProvider>
+    <div className="app">
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </div>
   );
 }
 
