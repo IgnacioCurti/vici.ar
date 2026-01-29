1714642b4645d3689696f4ae5c13b1cf4a86299b
Kevin Masci <kevin-sor@hotmail.com>
2026-01-29 19:25:58 -0300

    Add Authentication functionality

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { api } from "./api";
+import type { RegisterData, AuthResponse, LoginData } from "../types/auth.types";
+
+export const authService = {
+  async register(data: RegisterData): Promise<AuthResponse> {
+    const response = await api.post<AuthResponse>('/auth/register', {
+      email: data.email,
+      username: data.username,
+      password: data.password,
+      ...(data.displayName && {displayName: data.displayName}),
+      ...(data.description && {displayName: data.description})
+    });
+    return response.data;
+  },
+
+  async verifyEmail(userId: number, code: string): Promise<void> {
+    await api.get(`/auth/verify-email/${userId}/${code}`);
+  },
+
+  async login(data: LoginData): Promise<AuthResponse> {
+    const response = await api.post<AuthResponse>('/auth/login', data);
+    return response.data;
+  },
+
+  async logout(): Promise<void> {
+    // endpoint en el back? api.post('/auth/logout')
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+}
