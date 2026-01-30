b9cbcb49e96702e62b39361e7c9f5bad1b8dbb32
Kevin Masci <kevin-sor@hotmail.com>
2026-01-30 02:22:19 -0300

    Add verify email

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AppNavbar from "./app/components/layout/navbar";
 import Home from "./app/components/pages/Home";
+import VerifyEmail from "./app/components/auth/VerifyEmail";
 // import Login from "./app/pages/Login";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <AppNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
         {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </div>
