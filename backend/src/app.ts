bd838f288929381fed20b5d9f5880b8692cf1d9d
Kevin Masci <kevin-sor@hotmail.com>
2026-01-29 23:08:57 -0300

    Configuration cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import router from './router.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-type', 'Authorization']
+}));
 
 app.use(express.json());
 
@@ -10,5 +18,6 @@ app.get("/health", (_req, res) => {
 });
 
 app.use('/api', router);
+
 
 export default app
